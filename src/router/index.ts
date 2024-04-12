de4d1e60c66befc5c28dc3f6ceaee73f3b17359a
Byran Zaugg <blzaugg@gmail.com>
2024-04-12 06:20:53 +0000

    Add all 3 steps to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,15 @@
+/*
+ * Generated file.
+ * 
+ * I then:
+ *  - updated/added routes
+ */
+
 import { createRouter, createWebHistory } from 'vue-router'
-import MoviesListView from '../views/MoviesListView.vue'
-import AddNewMovie1View from '../views/AddNewMovie1View.vue'
+import MoviesListView from '@/views/MoviesListView.vue'
+import AddNewMovie1View from '@/views/AddNewMovie1View.vue'
+import AddNewMovie2View from '@/views/AddNewMovie2View.vue'
+import AddNewMovie3View from '@/views/AddNewMovie3View.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +23,16 @@ const router = createRouter({
       path: '/add-new-movie-1',
       name: 'add-new-movie-1',
       component: AddNewMovie1View
+    },
+    {
+      path: '/add-new-movie-2',
+      name: 'add-new-movie-2',
+      component: AddNewMovie2View
+    },
+    {
+      path: '/add-new-movie-3',
+      name: 'add-new-movie-3',
+      component: AddNewMovie3View
     }
   ]
 })
---
+++ src/views/AddNewMovie2View.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+/* Custom file. */
+</script>
+
+<template>
+  <main>
+    <h1>Add New Movie 2</h1>
+  </main>
+</template>
