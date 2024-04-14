9a3a794b2189c9e76a60b08142154eed9a3dd014
Byran Zaugg <blzaugg@gmail.com>
2024-04-14 19:47:42 +0000

    Fix eslint & prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,22 @@
-/* Generated file. */
+/*
+ * Generated file.
+ *
+ * I then:
+ *  - Fixed extends order.
+ *  - Added recommended configs
+ */
 
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
+  extends: [
     'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-typescript/recommended',
+    'plugin:prettier/recommended',
+    '@vue/eslint-config-prettier'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
