79784669090c1f0ba81818234d63990ce75ecb0b
Byran Zaugg <blzaugg@gmail.com>
2024-04-19 13:13:09 +0000

    Add shadcn Select

---
+++ src/components/shadcn/select/SelectItemText.vue
@@ -0,0 +1,13 @@
+<!-- Generated file. -->
+
+<script setup lang="ts">
+import { SelectItemText, type SelectItemTextProps } from 'radix-vue'
+
+const props = defineProps<SelectItemTextProps>()
+</script>
+
+<template>
+  <SelectItemText v-bind="props">
+    <slot />
+  </SelectItemText>
+</template>
