6c55e09329a3b483a830bc0c3f801218aaa21c9a
Byran Zaugg <blzaugg@gmail.com>
2024-04-19 11:13:09 +0000

    Hide active route navigation

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,27 @@
  * I then:
  *  - Updated nav
  */
-
-import { RouterLink, RouterView } from 'vue-router'
+import { computed } from 'vue'
+import { RouterLink, RouterView, useRoute } from 'vue-router'
 
 import { Button } from '@/components/shadcn/button'
+
+const route = useRoute()
+const underAddNewMovieFlow = computed(() => route.path.startsWith('/add-new-movie'))
 </script>
 
 <template>
   <header class="mb-3">
+    <!-- TODO: Rework navigation -->
     <nav>
-      <Button asChild>
+      <Button v-if="underAddNewMovieFlow" asChild>
         <RouterLink to="/">Movies List</RouterLink>
       </Button>
-      <!-- TODO: Hide button when in form flow -->
-      <Button asChild>
-        <RouterLink to="/add-new-movie/1">Add A New Movie</RouterLink>
+
+      <Button v-else asChild>
+        <RouterLink :to="{ name: 'add-new-movie-1', query: { reset: 'true' } }"
+          >Add A New Movie</RouterLink
+        >
       </Button>
     </nav>
   </header>
