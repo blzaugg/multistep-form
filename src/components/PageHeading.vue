e59052446e5b2ed408857c1a9a0a6b9c469cd20a
Byran Zaugg <blzaugg@gmail.com>
2024-04-20 18:22:47 +0000

    Add PageHeading

---
+++ src/components/PageHeading.vue
@@ -0,0 +1,22 @@
+<!-- Custom file. -->
+
+<!--
+  Usage example:
+
+  <PageHeading headingText="Add a New Movie" />
+-->
+
+<script setup lang="ts">
+defineProps<{
+  headingText: string
+}>()
+</script>
+
+<template>
+  <header class="my-2">
+    <!-- TODO: Customize page heading -->
+    <div class="prose dark:prose-invert">
+      <h1 class="text-2xl">{{ headingText }}</h1>
+    </div>
+  </header>
+</template>
