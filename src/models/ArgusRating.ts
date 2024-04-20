1b33da2e0e731caaad9ac57ab52bb43387124bd6
Byran Zaugg <blzaugg@gmail.com>
2024-04-20 18:10:41 +0000

    Add ArgusRating

--- src/models/ArgusRating.ts
+++ src/models/ArgusRating.ts
@@ -1,3 +1,34 @@
+/* Custom file. */
+
+/**
+ * TODO: NOT CURRENTLY IN USE.
+ */
+
+type DisplayOrder = 1 | 2 | 3 | 4 | 5 | 6
+
+export type Rating = 0 | 1 | 2 | 3 | 4 | 5
+
+interface RatingCategory<
+  LabelLongType extends string,
+  LabelShortType extends string,
+  DO extends DisplayOrder
+> {
+  displayOrder: DO
+  labelLong: LabelLongType
+  labelShort: LabelShortType
+  rating: Rating
+}
+
+export interface RatingCategories {
+  violence: RatingCategory<'Violence', 'V', 1>
+  gore: RatingCategory<'Gore', 'G', 2>
+  nudity: RatingCategory<'Nudity', 'N', 3>
+  sex: RatingCategory<'Sex', 'S', 4>
+  language: RatingCategory<'Language', 'L', 5>
+  drugs: RatingCategory<'Drugs', 'D', 6>
+}
+
 export interface ArgusRating {
-  // Nothing yet
+  categories: RatingCategories
+  // TODO: votesTotal
 }
