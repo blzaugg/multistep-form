9420cc72fc942701631c76a8607a942d38ea5ff4
Byran Zaugg <blzaugg@gmail.com>
2024-04-22 14:35:51 +0000

    Add primary variant to Toast

--- src/components/shadcn/toast/index.ts
+++ src/components/shadcn/toast/index.ts
@@ -21,6 +21,7 @@ export const toastVariants = cva(
     variants: {
       variant: {
         default: 'border bg-background text-foreground',
+        primary: 'border bg-primary text-primary-foreground',
         secondary: 'border bg-slate-900 text-secondary-foreground',
         destructive:
           'destructive group border-destructive bg-destructive text-destructive-foreground'
