2aaa8c993c2e54c7de81034d19b5929690dd8f43
Byran Zaugg <blzaugg@gmail.com>
2024-04-22 06:54:16 +0000

    Add shadcn Toast

---
+++ src/components/shadcn/toast/ToastProvider.vue
@@ -0,0 +1,13 @@
+<!-- Generated file. -->
+
+<script setup lang="ts">
+import { ToastProvider, type ToastProviderProps } from 'radix-vue'
+
+const props = defineProps<ToastProviderProps>()
+</script>
+
+<template>
+  <ToastProvider v-bind="props">
+    <slot />
+  </ToastProvider>
+</template>
